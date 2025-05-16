fedaf5a3a76aca9c965d543146e3dfd24ec09758
diorm47 <dior47m@gmail.com>
2025-05-16 15:20:59 +0500

    desctop

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,30 @@ document.querySelectorAll(".faq_item .faq_title").forEach((title) => {
     item.classList.toggle("active");
   });
 });
+
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+window.addEventListener("scroll", function () {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+
+  document.querySelector(".nav_line").style.width = scrollPercent + "%";
+});
+
+document.querySelectorAll(".scrollBtn").forEach((button) => {
+  button.addEventListener("click", function () {
+    const targetSelector = this.getAttribute("data-target");
+    const targetElement = document.querySelector(targetSelector);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
